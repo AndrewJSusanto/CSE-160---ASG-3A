2c2a8b2897820613f374b5e1ddd23e103c839560
Andrew <andrewjsusanto@gmail.com>
2024-05-04 20:24:17 -0700

    Global texture loading on server, x and y axis rotation logging

--- src/World.js
+++ src/World.js
@@ -7,11 +7,12 @@ var VSHADER_SOURCE =    `
   varying vec2 v_UV;
   uniform mat4 u_ModelMatrix;
   uniform mat4 u_GlobalRotateMatrix;
+  uniform mat4 u_GlobalRotateMatrixY;
   uniform mat4 u_ViewMatrix;
   uniform mat4 u_ProjectionMatrix;
   void main() {
     //gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
-    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotateMatrix * u_GlobalRotateMatrixY * u_ModelMatrix * a_Position;
     v_UV = a_UV;
   }`
 
@@ -36,11 +37,30 @@ let u_ModelMatrix;
 let u_ProjectionMatrix;
 let u_ViewMatrix;
 let u_GlobalRotateMatrix;
+let u_GlobalRotateMatrixY;
 let identityM;
 
 let cursorPosition = [0, 0];
 let g_globalAngle = 0;
+let g_globalAngleY = 0;
 let g_globalRot = 0;
+
+//
+let idleAnimate = false;
+let flapAnimate = false;
+let spinAnimate = false;
+let g_legAngle = -10;
+let g_earAngle = 40;
+let g_trunkAngle = -35;
+
+let g_trunk1Angle = -3;
+let g_trunk2Angle = -3;
+let g_trunk3Angle = -3;
+
+let g_tailAngle = 0;
+let g_headAngle = 0;
+let g_testAngle = 0;
+
 
 var g_startTime = performance.now()/1000.0;
 var g_seconds = performance.now()/1000.0 - g_startTime;
@@ -95,6 +115,12 @@ function connectVariablesToGLSL() {
     u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
     if (!u_GlobalRotateMatrix) {
         console.log('Failed to get the storage location of u_GlobalRotateMatrix');
+        return;
+    }
+
+    u_GlobalRotateMatrixY = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrixY');
+    if (!u_GlobalRotateMatrixY) {
+        console.log('Failed to get the storage location of u_GlobalRotateMatrixY');
         return;
     } 
 
@@ -154,14 +180,95 @@ function addActionsForHTMLUI() {
             dx = newpos[0] - currpos.x;
             dy = newpos[1] - currpos.y;
             g_globalAngle -= (dx * delta);
+            g_globalAngleY += (dy * delta);
             currpos.x = newpos[0];
             currpos.y = newpos[1];
             dx = 0;
             dy = 0;
-            document.getElementById('angleValue').innerText = Math.floor(g_globalAngle) % 360;
-            renderAllShapes();
+            document.getElementById('angleValue').innerText = Math.abs(Math.floor(g_globalAngle) % 360);
+            document.getElementById('angleValueY').innerText = Math.abs(Math.floor(g_globalAngleY) % 360);
         }
     }
+
+    // Shift detection
+    canvas.addEventListener("click", function (e) {
+        if (e.shiftKey) {
+            spinAnimate = true;
+            document.getElementById('currentAnimation').innerText = "You Can't See Me"
+            document.getElementById('elephName').innerText = "JOHNNNNN CENAAAAAAA"
+        }
+    })
+
+
+    // Leg Segment Slider
+    document.getElementById('legSlider').addEventListener('mousemove',
+        function() {g_legAngle = this.value; renderAllShapes(); });
+
+    // Ear Segment Slider
+    document.getElementById('earSlider').addEventListener('mousemove',
+        function() {g_earAngle = this.value; renderAllShapes(); })
+
+    // Trunk Whole Slider
+    document.getElementById('trunkSlider').addEventListener('mousemove',
+        function() {g_trunkAngle = this.value; renderAllShapes(); })
+
+    // Trunk Segments Sliders
+    document.getElementById('trunk1Slider').addEventListener('mousemove',
+        function() {g_trunk1Angle = this.value; renderAllShapes(); })
+    document.getElementById('trunk2Slider').addEventListener('mousemove',
+        function() {g_trunk2Angle = this.value; renderAllShapes(); })
+    document.getElementById('trunk3Slider').addEventListener('mousemove',
+        function() {g_trunk3Angle = this.value; renderAllShapes(); })
+
+    // Tail Slider
+    document.getElementById('tailSlider').addEventListener('mousemove',
+        function() {g_tailAngle = this.value; renderAllShapes(); })
+
+    // Head Segment Slider
+    document.getElementById('headSlider').addEventListener('mousemove',
+        function() {g_headAngle = this.value; renderAllShapes(); })
+
+    // Buttons
+    // idleButton
+    idleAnimation = document.getElementById('idleButton');
+    idleAnimation.addEventListener('click', function (e) {
+        idleAnimate = true;
+        if(flapAnimate) {
+            flapAnimate = false;
+        }
+        if(spinAnimate) {
+            spinAnimate = false;
+        }
+    })
+    // flapButton
+    flapAnimation = document.getElementById('flapButton');
+    flapAnimation.addEventListener('click', function (e) {
+        flapAnimate = true;
+        if(idleAnimate) {
+            idleAnimate = false
+        }
+        if(spinAnimate) {
+            spinAnimate = false;
+        }
+    })
+    // clearAnimations
+    clearAnimations = document.getElementById('clearButton').addEventListener('click', function (e) {
+        flapAnimate = false;
+        idleAnimate = false;
+        spinAnimate = false;
+
+        g_legAngle = -10;
+        g_earAngle = 15;
+        g_trunkAngle = -20;
+        g_tailAngle = 0;
+        g_headAngle = 0;
+        g_testAngle = 0;
+        g_trunk1Angle = -3;
+        g_trunk2Angle = -3;
+        g_trunk3Angle = -3;
+            // leg, ear trunk, t1, t2, t3, tail, head, 
+        document.getElementById('sliderForm').reset();
+    })
 
 }
 
@@ -176,9 +283,9 @@ function main() {
 
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    renderScene();
+    //renderScene();
     
-    // requestAnimationFrame(tick);
+    requestAnimationFrame(tick);
 }
 
 function click(ev) {
@@ -196,51 +303,101 @@ function convertCoordinatesEventToGL(ev) {
     return ([x,y]);
 }
 
-// function tick() {
-//     // Update
-//     g_seconds = performance.now()/1000.0 - g_startTime;
-//     renderScene();
-//     requestAnimationFrame(tick);
-// }
+function tick() {
+    // Update
+    g_seconds = performance.now()/1000.0 - g_startTime;
+    renderScene();
+    requestAnimationFrame(tick);
+    animate();
+}
 
 function renderScene() {
     renderAllShapes();
 }
 
-function renderAllShapes() {
-    console.log("renderAllShapes called");
-    var startTime = performance.now();
+// function renderAllShapes() {
+//     console.log("renderAllShapes called");
+//     var startTime = performance.now();
 
-    var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
-    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
-    var viewMat = identityM;
-    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements)
-    var projMat = identityM;
-    gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
+//     var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+//     gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+//     var viewMat = identityM;
+//     gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements)
+//     var projMat = identityM;
+//     gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+//     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var body = new Cube();
-    body.color = [1, 0, 0, 1];
-    body.matrix.translate(-.25, -.5, 0.0);
-    body.matrix.scale(0.5, 1, .5);
-    body.render();
+//     var body = new Cube();
+//     body.color = [1, 0, 0, 1];
+//     body.matrix.translate(-.25, -.5, 0.0);
+//     body.matrix.scale(0.5, 1, .5);
+//     body.render();
 
-    var leftArm = new Cube();
-    leftArm.color = [1, 1, 0, 1];
-    leftArm.matrix.translate(0.7, 0, 0.0);
-    leftArm.matrix.rotate(45, 0, 0, 1);
-    leftArm.matrix.scale(0.25, 0.7, 0.5);
-    leftArm.render();
+//     var leftArm = new Cube();
+//     leftArm.color = [1, 1, 0, 1];
+//     leftArm.matrix.translate(0.7, 0, 0.0);
+//     leftArm.matrix.rotate(45, 0, 0, 1);
+//     leftArm.matrix.scale(0.25, 0.7, 0.5);
+//     leftArm.render();
 
-    var duration = performance.now() - startTime;
-    sendTextToHTML(" ms: " + Math.floor(duration) +
-                    " fps: " + Math.floor(10000/duration),
-                    "numdot");
+//     var duration = performance.now() - startTime;
+//     sendTextToHTML(" ms: " + Math.floor(duration) +
+//                     " fps: " + Math.floor(10000/duration),
+//                     "numdot");
 
 
+// }
+
+function animate() {
+    if(idleAnimate) {
+        idle();
+    }
+    if(flapAnimate) {
+        flap();
+    }
+    if(spinAnimate) {
+        spin();
+    }
 }
 
+function idle() {
+    g_headAngle = 10 * Math.cos(g_seconds);
+    g_trunkAngle = 5 * Math.sin(g_seconds);
+    g_tailAngle = 30 * Math.sin(g_seconds);
+    g_legAngle = 5 * Math.sin(g_seconds / 2);
+}
+
+function flap() {
+    g_earAngle = 30 * Math.cos(g_seconds * 2.1);
+    g_headAngle = 10 * Math.cos(g_seconds * 2);
+    g_trunkAngle = 10 * Math.sin(g_seconds * 2);
+    g_legAngle = -10 * Math.abs(Math.sin(g_seconds));
+    g_tailAngle = 1080 * Math.sin(g_seconds) * 2.5;
+}
+
+function spin() {
+    g_trunkAngle = 45;
+    g_legAngle = -50 * - Math.abs(Math.sin(g_seconds * 5));
+    setTimeout(() => {
+        spinAnimate = false;
+        let reset = document.getElementById('clearButton');
+        if(idleAnimate) {
+            setTimeout(() => {
+                document.getElementById('idleButton').click();
+            }, 130)
+        }
+        else if (flapAnimate) {
+            setTimeout(() => {
+                document.getElementById('flapButton').click();
+            }, 130)
+        }
+        else {
+            reset.click();
+        }
+        document.getElementById('elephName').innerText = "Smarto the Elephant.";
+    }, 1500);
+}
 
 
 function sendTextToHTML(text, htmlID) {
---
+++ src/build.js
@@ -0,0 +1,359 @@
+function renderAllShapes() {
+    // Clear <canvas>
+    var startTime = performance.now();
+
+    // Pass the matrix to u_ModelMatrix attribute
+    var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+    var globalRotMatY = new Matrix4().rotate(g_globalAngleY, 1, 0, 0);
+    gl.uniformMatrix4fv(u_GlobalRotateMatrixY, false, globalRotMatY.elements);
+    var viewMat = identityM;
+    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements)
+    var projMat = identityM;
+    gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
+    // Clear depth buffer bit to prevent depth being leftover from prev. frames
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Body
+        // Torso
+        var torso = new Cube();
+        torso.color = [172/255, 213/255, 238/255, 1.0];
+        torso.matrix.translate(-0.25, -0.5, 0);
+        torso.matrix.rotate(1, 1, 0, 0);
+        var torsoCoordMatFLS = new Matrix4(torso.matrix);
+        var torsoCoordMatFRS = new Matrix4(torso.matrix);
+        var torsoCoordMatHead = new Matrix4(torso.matrix);
+        torso.matrix.scale(0.45, 0.45, 0.6);
+        var torsoCoordMatBackend = new Matrix4(torso.matrix);
+        // right multiply leads to order: scale then translate
+        torso.render();
+
+        // Backend
+        var backend = new Cube();
+        backend.color = [156/255, 189/255, 238/255, 1.0];
+        backend.matrix = torsoCoordMatBackend;
+        backend.matrix.translate(0.01, 0, 0.7);
+        // Make torso come up rather than backend
+        //backend.matrix.rotate(g_testAngle, 1, 0, 0);
+        backend.matrix.scale(0.98, 0.95, 0.5);
+        var backendCoordMatBLS = new Matrix4(backend.matrix);
+        var backendCoordMatBRS = new Matrix4(backend.matrix);
+        var backendCoordMatTC = new Matrix4(backend.matrix);
+        backend.matrix.rotate(3, 1, 0, 0);
+        backend.render();
+        // Tailend
+        var tailcap = new Cube();
+        tailcap.color = [156/255, 189/255, 238/255, 1.0];
+        tailcap.matrix = backendCoordMatTC;
+        tailcap.matrix.translate(0.06, 0.1, 0.7);
+        tailcap.matrix.rotate(-5, 1, 0, 0);
+        var tailcapCoordMat = new Matrix4(tailcap.matrix);
+        tailcap.matrix.scale(0.9, 0.8, 0.5);
+        tailcap.render();
+
+        // Head
+            // Skull
+        var head = new Cube();
+        head.color = [172/255, 213/255, 238/255, 1.0];
+        head.matrix = torsoCoordMatHead;
+        head.matrix.translate(0.225, 0.3, 0.1);
+        head.matrix.rotate(g_headAngle, 1, 0, 0);
+        var headCoordMat_earL = new Matrix4(head.matrix);
+        var headCoordMat_earR = new Matrix4(head.matrix);
+        var headCoordMat_eyeL = new Matrix4(head.matrix);
+        var headCoordMat_eyeR = new Matrix4(head.matrix);
+        var headCoordMat_trunk = new Matrix4(head.matrix);
+        head.matrix.scale(0.5, 0.5, 0.5);
+        head.matrix.translate(-0.5, 0, -0.6);
+        head.render();
+
+
+            // Ears
+        var earL = new Cube();
+        earL.color = [172/255, 213/255, 225/255, 1];
+        earL.matrix = headCoordMat_earL;
+        earL.matrix.translate(0.25, 0.5, 0);
+        earL.matrix.rotate(-g_earAngle, 0, 0, 1);
+        var earLCoordMat = new Matrix4(earL.matrix);
+        earL.matrix.scale(0.2, 0.1, 0.6);
+        earL.matrix.translate(-0.1, -0.5, -0.55);
+        earL.render();
+
+        var earLF = new Cube();
+        earLF.color = [172/255, 213/255, 225/255, 1];
+        earLF.matrix = earLCoordMat;
+        earLF.matrix.translate(0.05, -0.01, 0);
+        earLF.matrix.rotate(-g_earAngle * 0.8, 0, 0, 1);
+        earLF.matrix.rotate(-5, 0, 0, 1);
+        var earLFCoordMat = new Matrix4(earLF.matrix);
+        earLF.matrix.scale(0.5, 0.125, 0.8);
+        earLF.matrix.translate(0.1, -0.25, -0.55);
+        earLF.render();
+
+        var earLF2 = new Cube();
+        earLF2.color = [200/255, 106/255, 82/255, 1];
+        earLF2.matrix = earLFCoordMat;
+        earLF2.matrix.translate(0.4, 0, 0);
+        earLF2.matrix.rotate(-g_earAngle * 0.2, 0, 0, 1);
+        earLF2.matrix.scale(0.3, 0.08, 0.70);
+        earLF2.matrix.translate(0.1, -0.3, -0.55);
+        earLF2.render();
+
+        var earR = new Cube();
+        earR.color = [172/255, 213/255, 225/255, 1];
+        earR.matrix = headCoordMat_earR;
+        earR.matrix.translate(-0.21, 0.5, 0);
+        earR.matrix.rotate(g_earAngle, 0, 0, 1);
+        var earRCoordMat = new Matrix4(earR.matrix);
+        earR.matrix.scale(0.2, 0.1, 0.6);
+        earR.matrix.translate(-1.1, -0.5, -0.55);
+        earR.render();
+
+        var earRF = new Cube();
+        earRF.color = [172/255, 213/255, 225/255, 1];
+        earRF.matrix = earRCoordMat;
+        earRF.matrix.translate(-0.085, -0.01, 0);
+        earRF.matrix.rotate(g_earAngle * 0.8, 0, 0, 1);
+        earRF.matrix.rotate(5, 0, 0, 1);
+        var earRFCoordMat = new Matrix4(earRF.matrix);
+        earRF.matrix.scale(0.5, 0.125, 0.8);
+        earRF.matrix.translate(-1.1, -0.25, -0.55);
+        earRF.render();
+        
+
+        var earRF2 = new Cube();
+        earRF2.color = [200/255, 106/255, 82/255, 1];
+        earRF2.matrix = earRFCoordMat;
+        earRF2.matrix.translate(-0.4, 0, 0);
+        earRF2.matrix.rotate(g_earAngle * 0.2, 0, 0, 1);
+        earRF2.matrix.scale(0.3, 0.08, 0.7);
+        earRF2.matrix.translate(-1.1, -0.3, -0.55);
+        earRF2.render();
+
+            // Eyes
+        var eyeL = new Cube();
+        eyeL.color = [0, 0, 0, 1];
+        eyeL.matrix = headCoordMat_eyeL;
+        eyeL.matrix.translate(-0.125, 0.205, 0.01);
+        eyeL.matrix.scale(0.05, 0.09, 0.3);
+        eyeL.matrix.translate(-0.5, 0, -1.1);
+        var eyeLCoordMat_glint = new Matrix4(eyeL.matrix);
+        eyeL.render();
+
+        var eyeLG = new Cube();
+        eyeLG.color = [1, 1, 1, 1];
+        eyeLG.matrix = eyeLCoordMat_glint;
+        eyeLG.matrix.translate(0.7, 0.5, 0.03);
+        eyeLG.matrix.scale(0.3, 0.3, 0.1);
+        eyeLG.matrix.translate(-0.5, 0, -0.5);
+        eyeLG.render();
+
+        var eyeR = new Cube();
+        eyeR.color = [0, 0, 0, 1];
+        eyeR.matrix = headCoordMat_eyeR;
+        eyeR.matrix.translate(0.125, 0.205, 0.01);
+        eyeR.matrix.scale(0.05, 0.09, 0.3);
+        eyeR.matrix.translate(-0.5, 0, -1.1);
+        var eyeRCoordMat_glint = new Matrix4(eyeR.matrix);
+        eyeR.render();
+
+        var eyeRG = new Cube();
+        eyeRG.color = [1, 1, 1, 1];
+        eyeRG.matrix = eyeRCoordMat_glint;
+        eyeRG.matrix.translate(0.7, 0.5, 0.03);
+        eyeRG.matrix.scale(0.3, 0.3, 0.1);
+        eyeRG.matrix.translate(-0.5, 0, -0.5)
+        eyeRG.render();
+
+            // Trunk: animate trunk
+        var trunk = new Cube();
+        trunk.color = [172/255, 213/255, 238/255, 1.0];
+        trunk.matrix = headCoordMat_trunk;
+        trunk.matrix.translate(0, 0.12, -0.3);
+        trunk.matrix.rotate(g_trunkAngle * 0.5, 1, 0, 0);
+        trunk.matrix.rotate(g_trunk1Angle, 1, 0, 0); 
+        trunk.matrix.rotate(-3, 1, 0, 0);
+        var trunk1CoordMat = new Matrix4(trunk.matrix);
+        trunk.matrix.scale(0.4, 0.2, 0.2);
+        trunk.matrix.translate(-0.5, -0.5, -0.5);
+        trunk.render();
+
+        var trunk2 = new Cube();
+        trunk2.color = [172/255, 213/255, 238/255, 1.0];
+        trunk2.matrix = trunk1CoordMat;
+        trunk2.matrix.rotate(g_trunkAngle, 1, 0, 0);
+        trunk2.matrix.rotate(g_trunk2Angle, 1, 0, 0);
+        trunk2.matrix.rotate(-3, 1, 0, 0);
+        var trunk2CoordMat = new Matrix4(trunk2.matrix);
+        var trunk2CoordMat_tint = new Matrix4(trunk2.matrix);
+        trunk2.matrix.translate(0, 0, 0);
+        trunk2.matrix.scale(0.35, 0.18, 0.4);
+        trunk2.matrix.translate(-0.5, -0.5, -0.85);
+        trunk2.render();
+
+        var trunkTint = new Cube();
+        trunkTint.color = [246/255, 106/255, 82/255, 1];
+        trunkTint.matrix = trunk2CoordMat_tint;
+        trunkTint.matrix.translate(0, 0.09, -0.2);
+        trunkTint.matrix.scale(0.36, 0.05, 0.1);
+        trunkTint.matrix.translate(-0.5, -0.5, -0.5);
+        trunkTint.render();
+
+        var trunk3 = new Cube();
+        trunk3.color = [90/255, 100/255, 100/255, 1.0];
+        trunk3.matrix = trunk2CoordMat;
+        trunk3.matrix.translate(0, -0.01, -0.3);
+        trunk3.matrix.rotate(g_trunkAngle * 0.9, 1, 0, 0);
+        trunk3.matrix.rotate(g_trunk3Angle, 1, 0, 0);
+        trunk3.matrix.scale(0.3, 0.12, 0.15);
+        trunk3.matrix.translate(-0.5, -0.5, -0.85);
+        trunk3.render();
+
+        // Front legs
+            // Front left - shoulder
+        var legFLS = new Cube();
+        legFLS.color = [172/255, 213/255, 238/255, 1.0];
+        legFLS.matrix = torsoCoordMatFLS;
+        legFLS.matrix.translate(0.45, 0.06, -0.02);
+        legFLS.matrix.rotate(-g_legAngle, 0, 0, 1);
+        legFLS.matrix.rotate(180, 0, 0, 1);
+        legFLS.matrix.rotate(-5, 1, 0, 1);
+        legFLS.matrix.rotate(5, 0, 0, 1);
+        var legFLS_CoordMat = new Matrix4(legFLS.matrix);
+        legFLS.matrix.translate(-0.05, -0.1, 0);
+        legFLS.matrix.scale(0.15, 0.35, 0.3);
+        legFLS.render();
+            // Front left - middle
+        var legFLM = new Cube();
+        legFLM.color = [156/255, 189/255, 238/255, 1.0];
+        legFLM.matrix = legFLS_CoordMat;
+        legFLM.matrix.translate(-0.06, 0.1, -0.04);
+        legFLM.matrix.rotate(2, 1, 0, 0);
+        legFLM.matrix.scale(0.175, 0.2, 0.35);
+        var legFLM_CoordMat = new Matrix4(legFLM.matrix);
+        legFLM.render();
+            // Front left - toe
+        var legFLT = new Cube();
+        legFLT.color = [106/255, 115/255, 115/255, 1];
+        legFLT.matrix = legFLM_CoordMat;
+        legFLT.matrix.translate(-0.1, 1, -0.1);
+        legFLT.matrix.scale(1.25, 0.5, 1.1);
+        legFLT.render();
+
+            // Front right - shoulder
+        var legFRS = new Cube();
+        legFRS.color = [172/255, 213/255, 238/255, 1.0];
+        legFRS.matrix = torsoCoordMatFRS;
+        legFRS.matrix.translate(0.05, 0.06, -0.02);
+        legFRS.matrix.rotate(g_legAngle, 0, 0, 1);
+        legFRS.matrix.rotate(180, 0, 0, 1);
+        legFRS.matrix.rotate(-5, 1, 0, 0);
+        legFRS.matrix.rotate(-1, 0, 0, 1);
+        legFRS_CoordMat = new Matrix4(legFRS.matrix);
+        legFRS.matrix.translate(-0.05, -0.1, 0);
+        legFRS.matrix.scale(0.15, 0.35, 0.3);
+        legFRS.render();
+            // Front right - middle
+        var legFRM = new Cube();
+        legFRM.color = [156/255, 189/255, 238/255, 1.0];
+        legFRM.matrix = legFRS_CoordMat;
+        legFRM.matrix.translate(-0.065, 0.1, -0.04);
+        legFRM.matrix.rotate(2, 1, 0, 0);
+        legFRM.matrix.scale(0.175, 0.2, 0.35);
+        var legFRM_CoordMat = new Matrix4(legFRM.matrix);
+        legFRM.render();
+            // Front right - toe
+        var legFRT = new Cube();
+        legFRT.color = [106/255, 115/255, 115/255, 1];
+        legFRT.matrix = legFRM_CoordMat;
+        legFRT.matrix.translate(-0.1, 1, -0.1);
+        legFRT.matrix.scale(1.25, 0.5, 1.1);
+        legFRT.render();
+
+        // Back legs
+            // Back left - shoulder (?)
+        var legBLS = new Cube();
+        legBLS.color = [172/255, 213/255, 238/255, 1.0];
+        legBLS.matrix = backendCoordMatBLS;
+        legBLS.matrix.translate(1.0, 0.4, 0.3);
+        var legBLSCoordMat = new Matrix4(legBLS.matrix);
+        legBLS.matrix.rotate(-g_legAngle, 0, 0, 1);
+        legBLS.matrix.rotate(180, 0, 0, 1);
+        legBLS.matrix.translate(-0.1, 0, 0);
+        legBLS.matrix.scale(0.4, 0.7, 0.9);
+        legBLS.render();
+            // Back left - middle
+        var legBLM = new Cube();
+        legBLM.color = [156/255, 189/255, 238/255, 1.0];
+        legBLM.matrix = legBLSCoordMat;
+        legBLM.matrix.rotate(-g_legAngle, 0, 0, 1);
+        var legBLMCoordMat = new Matrix4(legBLM.matrix);
+        legBLM.matrix.scale(0.5, 0.8, 1.1);
+        legBLM.matrix.translate(-0.7, -1.2, -0.1);
+        legBLM.render();
+            // Back left - toe
+        var legBLT = new Cube();
+        legBLT.color = [106/255, 115/255, 115/255, 1];
+        legBLT.matrix = legBLMCoordMat;
+        legBLT.matrix.translate(-0.4, -1.2, -0.15);
+        legBLT.matrix.scale(0.6, 0.5, 1.2);
+        legBLT.render();
+
+            // Back right - shoulder (?)
+        var legBRS = new Cube();
+        legBRS.color = [172/255, 213/255, 238/255, 1.0];
+        legBRS.matrix = backendCoordMatBRS;
+        legBRS.matrix.translate(0, 0.4, 0.3);
+        var legBRSCoordMat = new Matrix4(legBRS.matrix);
+        legBRS.matrix.rotate(g_legAngle, 0, 0, 1);
+        legBRS.matrix.rotate(180, 0, 0, 1);
+        legBRS.matrix.translate(-0.3, 0, 0);
+        legBRS.matrix.scale(0.4, 0.7, 0.9);
+        legBRS.render();
+            // Back right - middle
+        var legBRM = new Cube();
+        legBRM.color = [156/255, 189/255, 238/255, 1.0];
+        legBRM.matrix = legBRSCoordMat;
+        legBRM.matrix.rotate(g_legAngle, 0, 0, 1);
+        var legBRMCoordMat = new Matrix4(legBRM.matrix);
+        legBRM.matrix.scale(0.5, 0.8, 1.1);
+        legBRM.matrix.translate(-0.3, -1.2, -0.1);
+        legBRM.render();
+            // Back right - toe
+        var legBRT = new Cube();
+        legBRT.color = [106/255, 115/255, 115/255, 1];
+        legBRT.matrix = legBRMCoordMat;
+        legBRT.matrix.translate(-0.2, -1.2, -0.15);
+        legBRT.matrix.scale(0.6, 0.5, 1.2);
+        legBRT.render();
+
+
+
+        // Tail: animate tail as propeller? idk
+        
+        var tail = new Cube();
+        tail.color = [106/255, 115/255, 115/255, 1];
+        tail.matrix = tailcapCoordMat;
+        tail.matrix.translate(0.4, 0.5, -0.05);
+        tail.matrix.rotate(-20, 1, 0, 0);
+        tail.matrix.rotate(-g_tailAngle, 0, 0, 1);
+        var tailCoordMat = new Matrix4(tail.matrix);
+        tail.matrix.scale(0.06, -1.0, 0.1);
+        tail.matrix.translate(-0.5, 0, 5);
+        tail.render();
+
+        var tailEnd = new Cube();
+        tailEnd.color = [246/255, 106/255, 82/255, 1];
+        tailEnd.matrix = tailCoordMat;
+        tailEnd.matrix.translate(0, -1, 0.55);
+        tailEnd.matrix.scale(0.15, 0.15, 0.15);
+        tailEnd.matrix.translate(-0.5, -0.5, -0.5);
+        tailEnd.render();
+
+
+
+    var duration = performance.now() - startTime;
+    sendTextToHTML(" ms: " + Math.floor(duration) +
+                    " fps: " + Math.floor(10000/duration),
+                    "numdot");
+}
